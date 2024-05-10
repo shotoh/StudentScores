a940694b514d6d79e208cf01d24c552d45ce2696
shotoh <trueshotoh@gmail.com>
2024-05-09 18:59:30 -0700

    added comments

--- StudentSQL.py
+++ StudentSQL.py
@@ -1,9 +1,9 @@
 import sqlite3
 
-db = sqlite3.connect('./Student.db')
+db = sqlite3.connect('./Student.db')  # creates database
 cursor = db.cursor()
 query = ('''CREATE TABLE IF NOT EXISTS Ten_Students(ID INT NOT NULL PRIMARY KEY, First_name TEXT, Last_name TEXT, Midterm REAL, 
-        Final REAL, Project1 REAL, Project2 REAL, Grade REAL, Letter_Grade TEXT);''')
+        Final REAL, Project1 REAL, Project2 REAL, Grade REAL, Letter_Grade TEXT);''')  # creates table
 cursor.execute(query)
 query = ('''INSERT INTO Ten_Students VALUES
         ('111111', 'John', 'Doe', '85', '87.5', '92', '86', '87.89999999999999', 'B'),
@@ -15,14 +15,14 @@ query = ('''INSERT INTO Ten_Students VALUES
         ('111117', 'Jessica', 'Garcia', '82', '80.0', '86', '91', '85.5', 'B'),
         ('111118', 'Fred', 'Clark', '85', '100.0', '95', '96', '94.3', 'A'),
         ('111119', 'Bob', 'Lopez', '56', '75.0', '80', '80', '74.2', 'C'),
-        ('111120', 'Anthony', 'White', '92', '100.0', '95', '92', '94.5', 'A');''')
+        ('111120', 'Anthony', 'White', '92', '100.0', '95', '92', '94.5', 'A');''')  # inserts all records in one INSERT query
 cursor.execute(query)
 db.commit()
-query = ('''SELECT * FROM Ten_Students ORDER BY First_name;''')
+query = '''SELECT * FROM Ten_Students ORDER BY First_name;'''  # order students by first names
 print(cursor.execute(query).fetchall())
-query = ('''SELECT Letter_Grade, COUNT(*) FROM Ten_Students GROUP BY Letter_Grade;''')
+query = '''SELECT Letter_Grade, COUNT(*) FROM Ten_Students GROUP BY Letter_Grade;'''  # selects each grade with the number of records matching grade
 print(cursor.execute(query).fetchall())
-query = ('''DELETE FROM Ten_Students WHERE Letter_Grade = 'D';''')
+query = '''DELETE FROM Ten_Students WHERE Letter_Grade = 'D';'''  # deletes all records where letter grade is D
 cursor.execute(query)
 db.commit()
 db.close()
